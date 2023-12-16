5edd938496265aa759322aaf98fc1c610008f4cd
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2023-12-16 17:44:46 +0300

    work app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {NotFound} from "./pages/NotFound";
+import {Header} from "./components/Header";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {About} from "./pages/About";
+import {Footer} from "./components/Footer";
+import {NewNote} from "./pages/NewNote";
+import {UpdateNote} from "./pages/UpdateNote";
+import {Home} from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router basename='/my-notes'>
+            <Header/>
+            <main className="container content">
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path='/note/find-one/:id' component={UpdateNote}/>
+                    <Route path='/note/new-note' component={NewNote}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </main>
+            <Footer/>
+        </Router>
+    )
 }
 
 export default App;
